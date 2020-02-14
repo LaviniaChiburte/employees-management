03c045f905f2ed6531c3467913a2209d8fe082c1
Lavinia Chiburte <lavinia.chiburte@gmail.com>
2020-02-14 10:00:27 +0200

    first part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import NavBar from "./components/NavBar";
+// import Employee from "./components/Employee";
+import EmployeeList from "./components/EmployeeList";
+import EmployeeTable from "./components/EmployeeTable";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        {/* <EmployeesList /> */}
+        <Route path="/employees">
+          <EmployeeList />
+        </Route>
+        <Route path="/managing">
+          <EmployeeTable />
+        </Route>
+        <Redirect to="/employees" />
+      </Switch>
+    </Router>
   );
 }
 
